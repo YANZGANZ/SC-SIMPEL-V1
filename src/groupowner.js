216a5f06438ae5773a47ce0b3fc4580879066340
Dapabot <101037424+Dapabot@users.noreply.github.com>
2022-03-25 16:57:57 +0700

    Update groupowner.js

--- src/groupowner.js
+++ src/groupowner.js
@@ -1,9 +1,9 @@
 exports.groupowner = (prefix) => {
 return `*- LINK GROUP OWNER BOT*
 
-1. https://chat.whatsapp.com/G1PlSE3VR6e99hmMaxzpkg
-2. https://chat.whatsapp.com/LV3DOUhJerlHBkvAjcB5zr
-3. https://chat.whatsapp.com/LNCchYC5Iyp2VLpqy0oK7C
+1. https://chat.whatsapp.com/BLMYFHwQkp89v8niubnSFN
+2. https://chat.whatsapp.com/BLMYFHwQkp89v8niubnSFN
+3. https://chat.whatsapp.com/Ch5rqamn6dE94g1L5ih4uM
 
 _Jika mau bergabung jangan lupa baca deskripsi!!_`
 }
